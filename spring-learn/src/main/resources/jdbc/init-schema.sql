85920fae0cb358154371b98ea5abd36848ca5d2c
WangHuanyu <why.DK37@gmail.com>
2016-03-26 20:05:00 +0800

    jdbc test

---
+++ spring-learn/src/main/resources/jdbc/init-schema.sql
@@ -0,0 +1,16 @@
+
+
+--测试数据
+create table user(
+  ID INT PRIMARY key,
+  NAME VARCHAR(20),
+  AGE INT
+);
+
+create table T_ACTOR(
+  ID INT PRIMARY key,
+  first_name VARCHAR(20),
+  last_name VARCHAR(20),
+  age INT,
+  specialty VARCHAR (20)
+);
