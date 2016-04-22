7e73fc3fc6262a881458aab153ecbb35bac6c42b
why <why.DK37@gmail.com>
2016-04-22 08:35:44 +0800

    dbcp 1.4

--- spring-learn/src/main/java/c0/Test.java
+++ spring-learn/src/main/java/c0/Test.java
@@ -2,6 +2,8 @@ package c0;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by drug on 2016/4/15.
@@ -14,5 +16,14 @@ public class Test {
         BigInteger bigInteger = new BigInteger("234");
         bigInteger = new BigInteger("123456789987654321123456789987654321123456789987654321");
         System.out.println(bigInteger);
+
+        //保持引用，防止自动垃圾回收
+        List<String> list = new ArrayList<String>(2000);
+        int i = 0;
+        while(true){
+            //通过intern方法向常量池中手动添加常量
+            list.add(String.valueOf("通过intern方法向常量池中手动添加常量"+i++).intern());
+            System.out.println(list.get(list.size()-1));
+        }
     }
 }
