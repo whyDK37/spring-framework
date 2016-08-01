47f09b61d4e8e4d041ee9a3af7f4eb5b3e786ef5
wanghuanyu <why.DK37@gmail.com>
2016-08-01 16:36:35 +0800

    迁移

---
+++ spring-learn/src/main/java/me/spring/chapt2/TestBean.java
@@ -0,0 +1,32 @@
+package me.spring.chapt2;
+
+/**
+ * Hello world!
+ *
+ */
+public class TestBean  extends Abc{
+
+
+    private TestBean testBean;
+    private String hello ="Hello World!";
+    public void hello()
+    {
+        System.out.println( hello );
+    }
+
+    public void setTestBean(TestBean testBean) {
+        this.testBean = testBean;
+    }
+
+    public TestBean getTestBean() {
+        return testBean;
+    }
+
+    public void setHello(String hello) {
+        this.hello = hello;
+    }
+
+    public void test(){
+        System.out.println(this.hello+"...");
+    }
+}
